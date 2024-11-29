90c9081d5e57bc28bfabc49b3c2e4bb0d910ac8b
Dylan J. Gerrits <gerritsdylan@gmail.com>
2024-11-29 17:53:10 +0100

    Scanning: Parentheses

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -5,6 +5,23 @@ import (
 	"os"
 )
 
+type Scanner struct{}
+
+func (scanner *Scanner) Parentheses(content string) {
+	const (
+		LEFT  rune = '('
+		RIGHT rune = ')'
+	)
+	for _, character := range content {
+		switch character {
+		case LEFT:
+			fmt.Println("LEFT_PAREN ( null")
+		case RIGHT:
+			fmt.Println("RIGHT_PAREN ) null")
+		}
+	}
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
@@ -22,14 +39,16 @@ func main() {
 	}
 
 	filename := os.Args[2]
-	fileContents, err := os.ReadFile(filename)
+	content, err := os.ReadFile(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
 		os.Exit(1)
 	}
 
-	if len(fileContents) > 0 {
-		panic("Scanner not implemented")
+	if len(content) > 0 {
+		scanner := &Scanner{}
+		scanner.Parentheses(string(content))
+		fmt.Println("EOF  null")
 	} else {
 		fmt.Println("EOF  null") // Placeholder, remove this line when implementing the scanner
 	}

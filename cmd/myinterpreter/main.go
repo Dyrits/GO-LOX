75900fccf90bbb9ab14688a4d90ab665bdd809cc
Dylan J. Gerrits <gerritsdylan@gmail.com>
2024-12-05 09:06:23 +0100

    Re-factorization

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -22,160 +22,221 @@ func (scanner *Scanner) PrintTokens() {
 	fmt.Println("EOF  null")
 }
 
-func (scanner *Scanner) TokenizeSingle(character rune) {
-	singles := map[rune]string{
-		'(': "LEFT_PAREN",
-		')': "RIGHT_PAREN",
-		'-': "MINUS",
-		'*': "STAR",
-		'+': "PLUS",
-		',': "COMMA",
-		'.': "DOT",
-		';': "SEMICOLON",
-		'{': "LEFT_BRACE",
-		'}': "RIGHT_BRACE",
-		'=': "EQUAL",
-		'!': "BANG",
-		'>': "GREATER",
-		'<': "LESS",
-		'/': "SLASH",
-	}
+func (scanner *Scanner) BreakLine(content string) {
+	if scanner.index < len(content) {
+		character := rune(content[scanner.index])
 
-	if name, exists := singles[character]; exists {
-		token := fmt.Sprintf("%s %c null", name, character)
-		fmt.Println(token)
-		scanner.tokens = append(scanner.tokens, token)
-	}
-}
-
-func (scanner *Scanner) HandleUnexpected(character rune) {
-	errors := map[rune]string{
-		'#': "HASH",
-		'$': "DOLLAR",
-		'%': "PERCENT",
-		'@': "AT",
-	}
-
-	if _, exists := errors[character]; exists {
-		fmt.Fprintln(os.Stderr, fmt.Sprintf("[line %d] Error: Unexpected character: %c", scanner.line, character))
-		scanner.exception = true
-	}
-}
-
-func (scanner *Scanner) Scan(content string) {
-
-	doubles := map[string]string{
-		"==": "EQUAL_EQUAL",
-		"!=": "BANG_EQUAL",
-		">=": "GREATER_EQUAL",
-		"<=": "LESS_EQUAL",
-	}
-
-	comments := map[string]string{
-		"//": "COMMENT",
-	}
-
-	whitespaces := map[rune]string{
-		' ':  "SPACE",
-		'\t': "TAB",
-	}
-
-	wrappers := map[rune]string{
-		'"': "STRING",
-	}
-
-	for index := 0; index < len(content); index++ {
-		character := rune(content[index])
-
-		// Check for new lines.
 		if character == '\n' {
 			scanner.line++
-			continue
+			scanner.index++
 		}
-		// Skip whitespaces.
+	}
+}
+
+func (scanner *Scanner) Skip(content string) {
+	if scanner.index < len(content) {
+		character := rune(content[scanner.index])
+		whitespaces := map[rune]string{
+			' ':  "SPACE",
+			'\t': "TAB",
+		}
+
 		if _, exists := whitespaces[character]; exists {
-			continue
+			scanner.index++
 		}
-		// Check for numbers.
-		if unicode.IsDigit(character) {
-			start := index
-			for index < len(content) && unicode.IsDigit(rune(content[index])) {
-				index++
+	}
+}
+
+func (scanner *Scanner) Comment(content string) {
+	if scanner.index < len(content) {
+		comments := map[string]string{
+			"//": "COMMENT",
+		}
+
+		if (scanner.index + 1) < len(content) {
+			characters := content[scanner.index : scanner.index+2]
+
+			if _, exists := comments[characters]; exists {
+
+				for scanner.index < len(content) && content[scanner.index] != '\n' {
+					scanner.index++
+				}
+
+				scanner.line++
+				scanner.index++
 			}
-			if index < len(content) && content[index] == '.' {
-				index++
-				for index < len(content) && unicode.IsDigit(rune(content[index])) {
-					index++
+		}
+	}
+}
+
+func (scanner *Scanner) TokenizeNumber(content string) {
+	if scanner.index < len(content) {
+		character := rune(content[scanner.index])
+
+		if unicode.IsDigit(character) {
+			start := scanner.index
+
+			for scanner.index < len(content) && unicode.IsDigit(rune(content[scanner.index])) {
+				scanner.index++
+			}
+
+			if scanner.index < len(content) && content[scanner.index] == '.' {
+				scanner.index++
+				for scanner.index < len(content) && unicode.IsDigit(rune(content[scanner.index])) {
+					scanner.index++
 				}
 			}
-			number := content[start:index]
+
+			number := content[start:scanner.index]
 			float, _ := strconv.ParseFloat(number, 64)
+
 			if math.Mod(float, 1.0) == 0 {
 				number = strconv.FormatFloat(float, 'f', -1, 64) + ".0"
 			} else {
 				number = strconv.FormatFloat(float, 'f', -1, 64)
 			}
-			token := fmt.Sprintf("NUMBER %s %s", content[start:index], number)
+
+			token := fmt.Sprintf("NUMBER %s %s", content[start:scanner.index], number)
 			fmt.Println(token)
 			scanner.tokens = append(scanner.tokens, token)
-			index--
-			continue
 		}
-		// Check for identifiers.
+	}
+
+}
+
+func (scanner *Scanner) TokenizeIdentifier(content string) {
+	if scanner.index < len(content) {
+		character := rune(content[scanner.index])
+
 		if unicode.IsLetter(character) || character == '_' {
-			start := index
-			for index < len(content) && (unicode.IsLetter(rune(content[index])) || unicode.IsDigit(rune(content[index])) || rune(content[index]) == '_') {
-				index++
+			start := scanner.index
+
+			for scanner.index < len(content) && (unicode.IsLetter(rune(content[scanner.index])) || unicode.IsDigit(rune(content[scanner.index])) || rune(content[scanner.index]) == '_') {
+				scanner.index++
 			}
-			identifier := content[start:index]
+
+			identifier := content[start:scanner.index]
 			token := fmt.Sprintf("IDENTIFIER %s null", identifier)
 			fmt.Println(token)
 			scanner.tokens = append(scanner.tokens, token)
-			index--
-			continue
 		}
-		if (index + 1) < len(content) {
-			characters := content[index : index+2]
-			// Check for comments.
-			if _, exists := comments[characters]; exists {
-				// Skip the rest of the line.
-				for index < len(content) && content[index] != '\n' {
-					index++
-				}
-				scanner.line++
-				continue
-			}
-			// Check for double characters.
+	}
+
+}
+
+func (scanner *Scanner) TokenizeDouble(content string) {
+	if scanner.index < len(content) {
+		doubles := map[string]string{
+			"==": "EQUAL_EQUAL",
+			"!=": "BANG_EQUAL",
+			">=": "GREATER_EQUAL",
+			"<=": "LESS_EQUAL",
+		}
+
+		if (scanner.index + 1) < len(content) {
+			characters := content[scanner.index : scanner.index+2]
+
 			if name, exists := doubles[characters]; exists {
 				token := fmt.Sprintf("%s %s null", name, characters)
 				fmt.Println(token)
 				scanner.tokens = append(scanner.tokens, token)
-				index++
-				continue
+				scanner.index += 2
+				// Recursive call to handle multiple double characters following each other.
+				scanner.TokenizeDouble(content)
 			}
 		}
-		// Check for wrappers.
+	}
+}
+
+func (scanner *Scanner) TokenizeString(content string) {
+	if scanner.index < len(content) {
+		character := rune(content[scanner.index])
+		wrappers := map[rune]string{
+			'"': "STRING",
+		}
+
 		if name, exists := wrappers[character]; exists {
-			// Get the rest of the string.
-			start := index
-			end := index + 1
+			start := scanner.index
+			end := scanner.index + 1
+
 			for end < len(content) && rune(content[end]) != character {
 				end++
 			}
+
 			if end == len(content) {
 				fmt.Fprintln(os.Stderr, fmt.Sprintf("[line %d] Error: Unterminated string.", scanner.line))
 				scanner.exception = true
-				break
+			} else {
+				token := fmt.Sprintf("%s %s %s", name, content[start:end+1], content[start+1:end])
+				fmt.Println(token)
+				scanner.tokens = append(scanner.tokens, token)
 			}
-			token := fmt.Sprintf("%s %s %s", name, content[start:end+1], content[start+1:end])
+
+			scanner.index = end + 1
+		}
+	}
+}
+
+func (scanner *Scanner) TokenizeSingle(content string) {
+	if scanner.index < len(content) {
+		character := rune(content[scanner.index])
+		singles := map[rune]string{
+			'(': "LEFT_PAREN",
+			')': "RIGHT_PAREN",
+			'-': "MINUS",
+			'*': "STAR",
+			'+': "PLUS",
+			',': "COMMA",
+			'.': "DOT",
+			';': "SEMICOLON",
+			'{': "LEFT_BRACE",
+			'}': "RIGHT_BRACE",
+			'=': "EQUAL",
+			'!': "BANG",
+			'>': "GREATER",
+			'<': "LESS",
+			'/': "SLASH",
+		}
+
+		if name, exists := singles[character]; exists {
+			token := fmt.Sprintf("%s %c null", name, character)
 			fmt.Println(token)
 			scanner.tokens = append(scanner.tokens, token)
-			index = end
+			scanner.index++
 		}
-		// Check for single characters.
-		scanner.TokenizeSingle(character)
-		// Check for errors.
-		scanner.HandleUnexpected(character)
+	}
+}
+
+func (scanner *Scanner) HandleUnexpected(content string) {
+	if scanner.index < len(content) {
+		character := rune(content[scanner.index])
+		errors := map[rune]string{
+			'#': "HASH",
+			'$': "DOLLAR",
+			'%': "PERCENT",
+			'@': "AT",
+		}
+
+		if _, exists := errors[character]; exists {
+			fmt.Fprintln(os.Stderr, fmt.Sprintf("[line %d] Error: Unexpected character: %c", scanner.line, character))
+			scanner.exception = true
+			scanner.index++
+		}
+	}
+}
+
+func (scanner *Scanner) Scan(content string) {
+
+	for scanner.index < len(content) {
+		scanner.BreakLine(content)
+		scanner.Skip(content)
+		scanner.Comment(content)
+		scanner.TokenizeNumber(content)
+		scanner.TokenizeIdentifier(content)
+		scanner.TokenizeDouble(content)
+		scanner.TokenizeString(content)
+		scanner.TokenizeSingle(content)
+		scanner.HandleUnexpected(content)
 	}
 	fmt.Println("EOF  null")
 	if scanner.exception {
